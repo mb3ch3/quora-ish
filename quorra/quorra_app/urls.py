d2317767d16e107ff61d9d850c850c375664e3b8
mb3ch3 <rhowdan@gmail.com>
2022-04-13 00:51:36 +0300

    adding authentication pages

--- quorra/quorra_app/urls.py
+++ quorra/quorra_app/urls.py
@@ -1,6 +1,9 @@
-
+from . import  views
 from django.urls import path
+from django.shortcuts import render
+app_name="quorra_app"
 
 urlpatterns = [
+    path('',views.home,name="home")
 
 ]
--- quorra/userprofiles/urls.py
+++ quorra/userprofiles/urls.py
@@ -1,6 +1,10 @@
-
 from django.urls import path
+from . import views
+
+app_name = "userprofiles"
 
 urlpatterns = [
-
+    path('',views.home,name="home"),
+    path('register/',views.userRegistration,name="register"),
+    path('login/',views.userLogin, name="login"),
 ]
